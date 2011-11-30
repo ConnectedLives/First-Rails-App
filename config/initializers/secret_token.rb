cc278e168f00db554149cb7e4bcbd5a49303bc1d
Connected Lives <connected.lives@gmail.com>
2011-11-30 14:57:28 +0530

    Initial Test Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'cd69324bbbf4a3ce98c70e5a717c1e22e961d5f8a6f1055fe3d3cce7c573a6e38559fc79e692028d73a26af5c8c31e89526c4c44f29f80ddb163a1de6c0cffdf'
